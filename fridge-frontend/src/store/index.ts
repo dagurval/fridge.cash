dd916e7abeb5d2de24d375ce898623a3adf866c7
Håvard Kittelsen <kittelsen@protonmail.com>
2021-01-17 03:30:37 +0100

    implemented state logic

--- fridge-frontend/src/store/index.ts
+++ fridge-frontend/src/store/index.ts
@@ -4,8 +4,59 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    bchNokPrice: 4213.37,
+    numberOfSlots: 24,
+    orderPriceNok: 600,
+    bchBalance: 0.011866985334779525,
+    soldUnits: 2,
+    profit: 0.1
+  },
+  getters: {
+    bchUnitPrice: state => {
+      const {
+        bchNokPrice,
+        numberOfSlots,
+        orderPriceNok,
+        bchBalance,
+        soldUnits,
+        profit
+      } = state
+    
+      const nokBalance = bchNokPrice * bchBalance
+      const priceNok = ((orderPriceNok - nokBalance) / (numberOfSlots - soldUnits) ) * (1 + profit)
+    
+      return priceNok/bchNokPrice
+    },
+    remainingUnits: state => {
+      return state.numberOfSlots-state.soldUnits
+    },
+    getNokBalance: state => {
+      return state.bchBalance*state.bchNokPrice
+    },
+    nokNeeded: state => {
+      return state.orderPriceNok - (state.bchBalance*state.bchNokPrice)
+    }
+  },
+  mutations: {
+    unitSold (state) {
+      state.soldUnits++
+    },
+    setBchBalance (state, payload:{balance:number}){
+      console.log(payload)
+      const {balance} = payload
+      state.bchBalance = balance
+    },
+    setBchNokPrice(state, payload:{price:number}) {
+      const { price } = payload
+      state.bchBalance = price
+    },
+    loadFridge(state, payload:{unitCount:number}){
+      const {unitCount} = payload
+      state.numberOfSlots = state.numberOfSlots - state.soldUnits + unitCount
+      state.soldUnits = 0
+    },
+  },
   actions: {},
   modules: {}
 });
--- fridge-frontend/src/views/Home.vue
+++ fridge-frontend/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="../assets/qr.png" />
+    <h3>Price {{bchPrice}}</h3>
+    <h3>Balance {{bchBalance}}</h3>
+    <h3>BalanceNok {{nokBalance}}</h3>
+    <h3>needed brfore buy {{nokNeeded}}</h3>
+    <h3>Units left: {{remainingUnits}}</h3>
+
+    <button v-on:click='setNewBalance'>asdasd</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    HelloWorld
+export default class Home extends Vue {
+
+  setNewBalance = () => {
+    this.$store.commit('setBchBalance', {balance:0.01})
   }
-})
-export default class Home extends Vue {}
+
+  get bchPrice() {
+    return this.$store.getters.bchUnitPrice
+  }
+  get bchBalance() {
+    return this.$store.state.bchBalance
+  }
+  get nokBalance() {
+    return this.$store.getters.getNokBalance
+  }
+  get remainingUnits() {
+    return this.$store.getters.remainingUnits
+  }
+  get nokNeeded() {
+    return this.$store.getters.nokNeeded
+  }
+}
 </script>
